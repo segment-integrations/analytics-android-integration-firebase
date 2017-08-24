b08bfcfa66ac25555ca74ca239140a4e4b1eab17
Brennan Gamwell <brennan@segment.com>
2017-08-24 16:54:04 -0700

    Update track method mapping

--- src/main/java/com/segment/analytics/android/integrations/firebase/FirebaseIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/firebase/FirebaseIntegration.java
@@ -3,9 +3,12 @@ package com.segment.analytics.android.integrations.firebase;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.analytics.FirebaseAnalytics.Event;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Properties;
 import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.integrations.IdentifyPayload;
@@ -14,9 +17,11 @@ import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.integrations.ScreenPayload;
 import com.segment.analytics.integrations.TrackPayload;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
+import static android.R.attr.format;
+import static com.segment.analytics.integrations.BasePayload.Type.identify;
 import static com.segment.analytics.internal.Utils.hasPermission;
 import static com.segment.analytics.internal.Utils.isNullOrEmpty;
 
@@ -107,5 +112,69 @@ public class FirebaseIntegration extends Integration<FirebaseAnalytics> {
   @Override
   public void track(TrackPayload track) {
     super.track(track);
+
+    // format event name
+    String event = track.event();
+    String eventName = mapEvent(event);
+
+    // format properties
+    Properties properties = track.properties();
+    Bundle mappedProperties = mapProperties(properties);
+
+    mFirebaseAnalytics.logEvent(eventName, mappedProperties);
+
+  }
+
+  private String mapEvent(String event) {
+
+    String eventName = event;
+
+    final Map<String, String> eventMapper = new HashMap<>();
+    eventMapper.put("Product Added", Event.ADD_TO_CART);
+    eventMapper.put("Checkout Started", Event.BEGIN_CHECKOUT);
+    eventMapper.put("Order Completed", Event.ECOMMERCE_PURCHASE);
+    eventMapper.put("Order Refunded", Event.PURCHASE_REFUND);
+    eventMapper.put("Product Viewed", Event.VIEW_ITEM);
+    eventMapper.put("Product List Viewed", Event.VIEW_ITEM_LIST);
+    eventMapper.put("Payment Info Entered", Event.ADD_PAYMENT_INFO);
+    eventMapper.put("Promotion Viewed", Event.PRESENT_OFFER);
+    eventMapper.put("Product Added to Wishlist", Event.ADD_TO_WISHLIST);
+    eventMapper.put("Product Shared", Event.SHARE);
+    eventMapper.put("Product Clicked", Event.SELECT_CONTENT);
+    eventMapper.put("Product Searched", Event.SEARCH);
+    eventMapper.put("Promotion Viewed", Event.PRESENT_OFFER);
+
+    if (eventMapper.containsKey(eventName)) {
+      eventName = eventMapper.get(eventName);
+    }
+
+    if (eventName.contains(" ")) {
+      eventName = eventName.trim().toLowerCase().replaceAll(" ", "_");
+    }
+
+    return eventName;
+
+  }
+
+  private Bundle mapProperties(Properties properties) {
+
+    // map properties to specced Firebase property names
+    // trim whitespace, replace spaces between words with underscores
+    final Map<String, String> propertiesMapper = new HashMap<>();
+    propertiesMapper.put("Product Added", Event.ADD_TO_CART);
+
+    Bundle bundle = new Bundle();
+
+    // refactor to check data type of each property value
+    // map specced Segment properties to specced Firebase Params
+
+    for (Map.Entry<String, Object> entry : properties.entrySet()) {
+      String property = entry.getKey();
+      String value = String.valueOf(entry.getValue());
+      bundle.putString(property, value);
+    }
+
+    return bundle;
+
   }
 }
