10558c22b855c9ae8aa38fa04139afe1b708a5ad
Brennan Gamwell <brennan@segment.com>
2017-08-23 17:29:14 -0700

    Updating identify method mapping

--- src/main/java/com/segment/analytics/android/integrations/firebase/FirebaseIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/firebase/FirebaseIntegration.java
@@ -4,8 +4,9 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 
-import com.google.firebase.analytics.FirebaseAnalytics; // FirebaseAnalytics class
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.integrations.IdentifyPayload;
 import com.segment.analytics.integrations.Integration;
@@ -13,13 +14,15 @@ import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.integrations.ScreenPayload;
 import com.segment.analytics.integrations.TrackPayload;
 
+import java.util.List;
+import java.util.Map;
+
 import static com.segment.analytics.internal.Utils.hasPermission;
 import static com.segment.analytics.internal.Utils.isNullOrEmpty;
-import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
 
 /**
- * Google Analytics for Firebase is a free app measurement solution that provides insight on
- * app usage and user engagement.
+ * Google Analytics for Firebase is a free app measurement solution that provides insight on app
+ * usage and user engagement.
  *
  * @see <a href="https://firebase.google.com/docs/analytics/">Google Analytics for Firebase</a>
  * @see <a href="#">Google Analytics for Firebase Integration</a>
@@ -27,68 +30,82 @@ import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
  */
 public class FirebaseIntegration extends Integration<FirebaseAnalytics> {
 
-  public static FirebaseAnalytics mFirebaseAnalytics;
+  public static final Factory FACTORY =
+      new Factory() {
+        @Override
+        public Integration<?> create(ValueMap settings, Analytics analytics) {
+          Logger logger = analytics.logger(FIREBASE_ANALYTICS_KEY);
+          if (!hasPermission(
+              analytics.getApplication(), Manifest.permission.ACCESS_NETWORK_STATE)) {
+            logger.debug("ACCESS_NETWORK_STATE is required for Firebase Analytics.");
+            return null;
+          }
+          if (!hasPermission(analytics.getApplication(), Manifest.permission.INTERNET)) {
+            logger.debug("INTERNET is required for Firebase Analytics.");
+            return null;
+          }
+          if (!hasPermission(analytics.getApplication(), Manifest.permission.WAKE_LOCK)) {
+            logger.debug("WAKE_LOCK is required for Firebase Analytics.");
+            return null;
+          }
 
-  public static final Factory FACTORY = new Factory() {
-    @Override public Integration<?> create(ValueMap settings, Analytics analytics) {
-      Logger logger = analytics.logger(FIREBASE_ANALYTICS_KEY);
-      if (!hasPermission(analytics.getApplication(), Manifest.permission.ACCESS_NETWORK_STATE)) {
-        logger.debug("ACCESS_NETWORK_STATE is required for Firebase Analytics.");
-        return null;
-      }
-      if (!hasPermission(analytics.getApplication(), Manifest.permission.INTERNET)) {
-        logger.debug("INTERNET is required for Firebase Analytics.");
-        return null;
-      }
-      if (!hasPermission(analytics.getApplication(), Manifest.permission.WAKE_LOCK)) {
-        logger.debug("WAKE_LOCK is required for Firebase Analytics.");
-        return null;
-      }
+          Context context = analytics.getApplication();
 
-      Context context = analytics.getApplication();
+          return new FirebaseIntegration(context);
+        }
 
-      return new FirebaseIntegration(context);
-    }
+        @Override
+        public String key() {
+          return FIREBASE_ANALYTICS_KEY;
+        }
+      };
 
-    @Override public String key() {
-      return FIREBASE_ANALYTICS_KEY;
-    }
-  };
+  private static final String FIREBASE_ANALYTICS_KEY = "Firebase";
 
-  private static final String FIREBASE_ANALYTICS_KEY = "Firebase Analytics";
+  final FirebaseAnalytics mFirebaseAnalytics;
 
   public FirebaseIntegration(Context context) {
     mFirebaseAnalytics = FirebaseAnalytics.getInstance(context);
   }
 
-  @Override public void onActivityStarted(Activity activity) {
+  @Override
+  public void onActivityStarted(Activity activity) {
     super.onActivityStarted(activity);
-
   }
 
-  @Override public void onActivityStopped(Activity activity) {
+  @Override
+  public void onActivityStopped(Activity activity) {
     super.onActivityStopped(activity);
-
   }
 
-  @Override public void identify(IdentifyPayload identify) {
+  @Override
+  public void identify(IdentifyPayload identify) {
     super.identify(identify);
 
     if (!isNullOrEmpty(identify.userId())) {
       mFirebaseAnalytics.setUserId(identify.userId());
-    }
 
-    // mFirebaseAnalytics.setUserProperty(key, value);
+      Map<String, Object> traits = identify.traits();
+
+      for (Map.Entry<String, Object> entry : identify.traits().entrySet()) {
+        String trait = entry.getKey();
+        String value = String.valueOf(entry.getValue());
+        mFirebaseAnalytics.setUserProperty(trait, value);
+      }
+
+    }
 
   }
 
-  @Override public void screen(ScreenPayload screen) {
+  @Override
+  public void screen(ScreenPayload screen) {
     super.screen(screen);
+
 //    mFirebaseAnalytics.setCurrentScreen(this, screen.name(), null /* class override */);
   }
 
-  @Override public void track(TrackPayload track) {
+  @Override
+  public void track(TrackPayload track) {
     super.track(track);
-
   }
 }
