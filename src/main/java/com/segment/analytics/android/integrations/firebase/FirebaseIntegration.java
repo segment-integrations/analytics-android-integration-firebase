ea1934c3dc431a76eb89bc81dfd761d85444ab7c
Brennan Gamwell <brennan@segment.com>
2017-08-27 16:59:01 -0700

    Refactoring

--- src/main/java/com/segment/analytics/android/integrations/firebase/FirebaseIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/firebase/FirebaseIntegration.java
@@ -1,15 +1,18 @@
 package com.segment.analytics.android.integrations.firebase;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.analytics.FirebaseAnalytics.Event;
 import com.segment.analytics.Analytics;
 import com.segment.analytics.Properties;
-import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.integrations.IdentifyPayload;
 import com.segment.analytics.integrations.Integration;
@@ -20,8 +23,6 @@ import com.segment.analytics.integrations.TrackPayload;
 import java.util.HashMap;
 import java.util.Map;
 
-import static android.R.attr.format;
-import static com.segment.analytics.integrations.BasePayload.Type.identify;
 import static com.segment.analytics.internal.Utils.hasPermission;
 import static com.segment.analytics.internal.Utils.isNullOrEmpty;
 
@@ -56,7 +57,7 @@ public class FirebaseIntegration extends Integration<FirebaseAnalytics> {
 
           Context context = analytics.getApplication();
 
-          return new FirebaseIntegration(context);
+          return new FirebaseIntegration(context, logger);
         }
 
         @Override
@@ -66,21 +67,30 @@ public class FirebaseIntegration extends Integration<FirebaseAnalytics> {
       };
 
   private static final String FIREBASE_ANALYTICS_KEY = "Firebase";
-
+  final Logger logger;
   final FirebaseAnalytics mFirebaseAnalytics;
 
-  public FirebaseIntegration(Context context) {
+  public FirebaseIntegration(Context context, Logger logger) {
+    this.logger = logger;
     mFirebaseAnalytics = FirebaseAnalytics.getInstance(context);
   }
 
   @Override
   public void onActivityStarted(Activity activity) {
     super.onActivityStarted(activity);
-  }
 
-  @Override
-  public void onActivityStopped(Activity activity) {
-    super.onActivityStopped(activity);
+    PackageManager packageManager = activity.getPackageManager();
+    try {
+      ActivityInfo info =
+              packageManager.getActivityInfo(activity.getComponentName(), PackageManager.GET_META_DATA);
+      CharSequence activityLabel = info.loadLabel(packageManager);
+
+      mFirebaseAnalytics.setCurrentScreen(activity, activityLabel.toString(), null /* class override */);
+      logger.verbose("mFirebaseAnalytics.setCurrentScreen(%s, %s, null /* class override */);", activity, activityLabel.toString());
+
+    } catch (PackageManager.NameNotFoundException e) {
+      throw new AssertionError("Activity Not Found: " + e.toString());
+    }
   }
 
   @Override
@@ -92,21 +102,21 @@ public class FirebaseIntegration extends Integration<FirebaseAnalytics> {
 
       Map<String, Object> traits = identify.traits();
 
-      for (Map.Entry<String, Object> entry : identify.traits().entrySet()) {
+      for (Map.Entry<String, Object> entry : traits.entrySet()) {
         String trait = entry.getKey();
+        trait = trait.trim().toLowerCase().replaceAll(" ", "_");
         String value = String.valueOf(entry.getValue());
         mFirebaseAnalytics.setUserProperty(trait, value);
+        logger.verbose("mFirebaseAnalytics.setUserProperty(%s, %s);", trait, value);
       }
-
     }
-
   }
 
   @Override
   public void screen(ScreenPayload screen) {
     super.screen(screen);
-
-//    mFirebaseAnalytics.setCurrentScreen(this, screen.name(), null /* class override */);
+    logger.verbose("Firebase Analytics does not support manual screen tracking. All screen views"
+            + "are collected automatically by their SDK");
   }
 
   @Override
@@ -122,7 +132,7 @@ public class FirebaseIntegration extends Integration<FirebaseAnalytics> {
     Bundle mappedProperties = mapProperties(properties);
 
     mFirebaseAnalytics.logEvent(eventName, mappedProperties);
-
+    logger.verbose("mFirebaseAnalytics.logEvent(%s, %s);", eventName, mappedProperties);
   }
 
   private String mapEvent(String event) {
@@ -153,28 +163,44 @@ public class FirebaseIntegration extends Integration<FirebaseAnalytics> {
     }
 
     return eventName;
-
   }
 
   private Bundle mapProperties(Properties properties) {
 
-    // map properties to specced Firebase property names
-    // trim whitespace, replace spaces between words with underscores
-    final Map<String, String> propertiesMapper = new HashMap<>();
-    propertiesMapper.put("Product Added", Event.ADD_TO_CART);
+    if (properties.value() != 0 && isNullOrEmpty(properties.currency())) {
+      logger.verbose("You must set `currency` in your event's property object to accurately pass 'value' to Firebase.");
+    }
 
     Bundle bundle = new Bundle();
 
-    // refactor to check data type of each property value
-    // map specced Segment properties to specced Firebase Params
-
     for (Map.Entry<String, Object> entry : properties.entrySet()) {
       String property = entry.getKey();
-      String value = String.valueOf(entry.getValue());
-      bundle.putString(property, value);
+
+      if (entry.getValue() instanceof Integer) {
+        int value = (int) entry.getValue();
+        bundle.putInt(property, value);
+        logger.verbose("bundle.putInt(%s, %s);", property, value);
+      }
+
+      if (entry.getValue() instanceof Double) {
+        double value = (double) entry.getValue();
+        bundle.putDouble(property, value);
+        logger.verbose("bundle.putInt(%s, %s);", property, value);
+      }
+
+      if (entry.getValue() instanceof Long) {
+        long value = (long) entry.getValue();
+        bundle.putLong(property, value);
+        logger.verbose("bundle.putInt(%s, %s);", property, value);
+      }
+
+      if (entry.getValue() instanceof String) {
+        String value = String.valueOf(entry.getValue());
+        bundle.putString(property, value);
+        logger.verbose("bundle.putInt(%s, %s);", property, value);
+      }
     }
 
     return bundle;
-
   }
 }
